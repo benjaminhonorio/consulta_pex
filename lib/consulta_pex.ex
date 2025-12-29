7dfd39a9be55e94bd6995e4f7e20516948d7c94c
Benjamin Honorio <benjaminhonorio.12@gmail.com>
2025-12-28 23:49:18 -0500

    feat: initial commit

---
+++ lib/consulta_pex.ex
@@ -0,0 +1,18 @@
+defmodule ConsultaPex do
+  @moduledoc """
+  Documentation for `ConsultaPex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ConsultaPex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/consulta_pex_test.exs
@@ -0,0 +1,8 @@
+defmodule ConsultaPexTest do
+  use ExUnit.Case
+  doctest ConsultaPex
+
+  test "greets the world" do
+    assert ConsultaPex.hello() == :world
+  end
+end
